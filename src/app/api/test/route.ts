4303447c8af71ff70203fd93d5422b53dd3f4bb3
dlord213 <lollingworld41@gmail.com>
2025-01-10 18:52:46 +0800

    app: ver 1.03

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,70 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
+export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
 
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const { data } = await axios.get(
+      "https://api.allorigins.win/get?url=" +
+        encodeURIComponent("https://www.gsmarena.com/")
+    );
+
+    if (!data.contents) {
+      return NextResponse.json(
+        { error: "Failed to fetch page content." },
+        { status: 500 }
+      );
+    }
+
+    const $ = cheerio.load(data.contents);
+
+    // Extract latest phones
+    const latestPhones: {
+      model: string;
+      image: string | undefined;
+      link: string | undefined;
+    }[] = [];
+    $(
+      '.module-latest:has(h4.section-heading:contains("Latest devices")) .module-phones-link'
+    ).each((_, el) => {
+      const model = $(el).text().trim();
+      const image = $(el).find("img").attr("src");
+      const link = $(el).attr("href")?.replace(".php", "");
+      latestPhones.push({ model, image, link });
+    });
+
+    // Extract phones in stores now
+    const phonesInStore: {
+      model: string;
+      image: string | undefined;
+      link: string | undefined;
+    }[] = [];
+    $(
+      '.module-latest:has(h4.section-heading:contains("In stores now")) .module-phones-link'
+    ).each((_, el) => {
+      const model = $(el).text().trim();
+      const image = $(el).find("img").attr("src");
+      const link = $(el).attr("href")?.replace(".php", "");
+      phonesInStore.push({ model, image, link });
+    });
+
+    const result = {
+      latestPhones,
+      phonesInStore,
+    };
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching latest phones data:", error);
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+    return NextResponse.json(
+      { error: "Failed to fetch data", details: errorMessage },
+      { status: 500 }
+    );
+  }
 }
