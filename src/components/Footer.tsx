3da8e973de6ca18eea23c4819e9f0de80e67598b
dlord213 <lollingworld41@gmail.com>
2025-01-12 17:11:15 +0800

    app: ver 1.08b

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-row bg-[#B03B48] p-4 rounded-tl-md rounded-tr-md justify-between h-full">
+    <footer className="flex flex-row bg-[#B03B48] lg:p-4 rounded-tl-md rounded-tr-md lg:justify-between h-full">
       <div className="flex flex-row gap-2 items-center">
         <img src="assets/logo/icon.png" className="w-full max-w-[72px]" />
-        <h1 className="font-bold lg:text-3xl text-white">SwitchDeals</h1>
+        <h1 className="font-bold lg:text-3xl text-white text-xl">
+          SwitchDeals
+        </h1>
       </div>
-      <div className="flex flex-col items-end justify-center">
+      <div className="lg:flex flex-col items-end justify-center hidden">
         <p className="text-white">
           made by{" "}
           <a href="https://github.com/dlord213" target="_blank">
