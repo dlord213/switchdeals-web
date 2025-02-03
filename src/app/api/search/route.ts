a653de0a858509f8f15faa0510dc77844501ae51
dlord213 <lollingworld41@gmail.com>
2025-02-03 16:09:34 +0800

    app: ver 1.10b (bug fixes0

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,18 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as cheerio from "cheerio";
-import { client } from "@/lib/client";
+import { createScraperClient } from "@/lib/client";
+
+const client = createScraperClient();
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("query");
+  const region = searchParams.get("region") || "";
+
+  await client.post("https://www.dekudeals.com/locale", `country=${region}`, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Origin: "https://www.dekudeals.com",
+      Referer: "https://www.dekudeals.com/",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "same-origin",
+    },
+    maxRedirects: 5,
+    validateStatus: (status) => status >= 200 && status < 400,
+  });
 
   if (!query) {
     return NextResponse.json({ error: "Missing query" }, { status: 400 });
   }
 
   try {
-    const url = `https://www.dekudeals.com/search?q=${query}`;
-    const { data } = await client.get(url);
+    const url = new URL("https://www.dekudeals.com/search");
+    url.searchParams.set("q", query);
+    if (region !== "us") {
+    }
+    const { data } = await client.get(url.toString(), {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+      },
+    });
 
     if (!data) return NextResponse.json({ games: [] }, { status: 500 });
 
@@ -25,6 +53,7 @@ export async function GET(request: NextRequest) {
       price: string;
       originalPrice: string;
       link: string | undefined;
+      region: string | undefined;
     }[] = [];
 
     $(".browse-cards.desktop > .row > .col").each((index, element) => {
@@ -50,6 +79,7 @@ export async function GET(request: NextRequest) {
         price,
         originalPrice,
         link,
+        region: region,
       });
     });
 
